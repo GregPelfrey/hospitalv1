568beaecfb30478738b400a81f2f5ed9a21a9d33
GregPelfrey <greg.pelfrey@gmail.com>
2015-01-21 20:57:43 -0500

    Hospital patient

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,45 @@
+class PatientsController < ApplicationController
+    def index
+      @patients = Patient.all
+    end
+
+    def new
+      @patient = Patient.new
+    end
+
+    def create
+      @patient = Patient.create patient_params
+      if @patient.save
+        flash[:notice]="New Patient Added"
+      redirect_to patients_path
+      else
+      flash[:error]="Save Unsuccessful"
+      redirect_to patients_path
+      render :new
+      end
+    end
+
+    def update
+      @patient = Patient.find params[:id]
+      @patient.update_attributes patient_params
+      redirect_to patients_path
+    end
+
+    def destroy
+      @patient = Patient.find params[:id]
+      @patient.delete
+      redirect_to patients_path
+    end
+
+private
+    def patient_params
+      params.require(:patient).permit(
+        :first_name,
+        :last_name,
+        :date_of_birth,
+        :description,
+        :gender,
+        :blood_type
+        )
+    end
+end
---
+++ app/models/patient.rb
@@ -0,0 +1,5 @@
+class Patient < ActiveRecord::Base
+
+  BLOOD_TYPE = ["O", "O+", "A", "A-", "B", "B-", "AB"]
+
+end
